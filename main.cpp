35a1141700498a7b1d32442e37f90ad4756d57ba
andreabocca <127606319+andreabocca@users.noreply.github.com>
2023-03-25 13:57:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+    //Qui vanno definite le variabili
 
-cin >> a >> b >> c;
+    float a=0,b=0,c=0,triangolo=0,quadrato=0,rettangolo=0,trapezio=0;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cin >> a >> b >> c;
 
+    //Qui va scritto il codice per eseguire i calcoli richiesti
 
+    triangolo=((a*b)/2);
+    quadrato=a*a;
+    rettangolo=a*b;
+    trapezio=(((a+b)*c)/2);
+
+    cout<<"Area triangolo e\'"<<" "<<triangolo<<endl;
+    cout<<"Area quadrato e\'"<<" "<<quadrato<<endl;
+    cout<<"Area rettanglo e\'"<<" "<<rettangolo<<endl;
+    cout<<"Area trapezio e\'"<<" "<<trapezio<<endl;
 }
